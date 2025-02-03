4bfb1fa4a93fd3d194ae83b167bfaeee6c092a2f
JoelNarvaez <joelnarmar20@gmail.com>
2025-02-03 13:43:15 -0600

    JS terminado

--- app.js
+++ app.js
@@ -1,1 +1,68 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let Amigos = []; // Array para almacenar los nombres
+
+function agregarAmigo() {
+    // Obtener el valor del input con id 'amigo'
+    const nuevoAmigo = document.getElementById('amigo').value;
+
+    // Validar que el valor no esta vacio
+    if (nuevoAmigo !== '') {
+        Amigos.push(nuevoAmigo); // Agrega el nuevo amigo al array
+        limpiar();
+        mostrarAmigos();
+        console.log(Amigos); // Muestra el array en la consola
+        return;
+    } else {
+        console.log('No puedes agregar un amigo vacio.');
+        alert('Por favor, inserte un nombre');
+        return;
+    }
+}
+
+function limpiar() {
+    document.querySelector('#amigo').value = ''; // Limpia el input
+    return;
+}
+
+function mostrarAmigos() {
+    const lista = document.getElementById('listaAmigos');
+
+    // Limpiar la lista para evitar duplicados
+    lista.innerHTML = '';
+
+    if (Amigos.length > 0) {
+        for (let amigo of Amigos) {
+            const elemento = document.createElement('li');
+            elemento.innerHTML = amigo;
+            lista.appendChild(elemento); // Agrega cada amigo como
+        }
+    } else {
+        alert('La lista de amigos esta vacia');
+        }
+
+    }
+
+
+    function sortearAmigo(){
+        let sorteado = document.querySelector('.button-draw');
+
+        if(Amigos.length < 1){
+            alert('No hay ningun amigo en la lista');
+            return;
+        }
+        else{
+            indice = random();
+            let seleccionado;
+            seleccionado = Amigos[indice];
+            sorteado.innerHTML = (`El sorteado es: ${seleccionado}`);
+            return;
+        }
+
+    }
+
+    function random(){
+        let numeroGenerado = Math.floor(Math.random()*Amigos.length);
+        return numeroGenerado;
+    }
+
+
+
